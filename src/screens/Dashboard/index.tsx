7dcdec9b1258d5cc5bd1de0889419f68cf7e746a
Thiago Galvao <thiagosilva128@gmail.com>
2022-07-13 13:52:19 -0300

    initial project

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {View, Text, StyleSheet} from "react-native"
+import {Container, Test} from "./style"
+
+export function Dashboard() {
+	return (
+		<Container>
+			<Test>Testando meu app</Test>
+		</Container>
+	)
+}
---
+++ src/screens/Dashboard/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native"
+
+export const Container = styled.View`
+	flex: 1;
+	align-items: center;
+	justify-content: center;
+	color: #fff;
+	background-color: #212;
+`
+
+export const Test = styled.Text`
+	font-size: 30px;
+	color: #fff;
+`
