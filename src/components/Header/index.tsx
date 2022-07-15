933019497f051d2fbf9fa950ea82030c9976016f
Thiago Galvao <thiagosilva128@gmail.com>
2022-07-15 01:48:04 -0300

    added header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import {Text} from "react-native"
+import {Container} from "./style"
+
+export function Header() {
+	return (
+		<Container>
+			<Text>"Teste"</Text>
+		</Container>
+	)
+}
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import {View, Text, StyleSheet} from "react-native"
 import {Container, Test} from "./style"
+import {Header} from "./../../components/Header"
 
 export function Dashboard() {
 	return (
 		<Container>
-			<Test>Testando meu app</Test>
+			<Header />
 		</Container>
 	)
 }
